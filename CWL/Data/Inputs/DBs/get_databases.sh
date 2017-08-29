3eae069734c56d7e65f99385b034a9756f0cf712
Andreas Wilke <wilke@mcs.anl.gov>
2017-08-29 10:29:55 -0500

    Added comments

--- CWL/Data/Inputs/DBs/get_databases.sh
+++ CWL/Data/Inputs/DBs/get_databases.sh
@@ -9,3 +9,12 @@ tar xf PhiX*Illumina*.tar.gz
 mv PhiX/Illumina/RTA/Sequence/Bowtie2Index/* Phix/  
 rm -rf Phix/Illumina
 
+
+# Silva version
+# wget  --no-check-certificate https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_128_SSURef_Nr99_tax_silva_trunc.fasta.gz
+# gunzip SILVA*.gz
+
+
+# Unite version
+# wget --no-check-certificate https://unite.ut.ee/sh_files/sh_mothur_release_s_20.11.2016.zip
+# unzip sh_mothur_release*.zip
