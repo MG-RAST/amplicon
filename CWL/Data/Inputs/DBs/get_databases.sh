1104b85bd0cef732fbe7cf40d1b337a7617dab36
Andreas Wilke <wilke@mcs.anl.gov>
2017-08-29 10:25:21 -0500

    Added Phix download

--- CWL/Data/Inputs/DBs/get_databases.sh
+++ CWL/Data/Inputs/DBs/get_databases.sh
@@ -2,3 +2,10 @@ wget http://download.microbiome.ch/silva_v128NR.zip
 wget http://download.microbiome.ch/unite.v72_dynamic.zip
 unzip silva_v128NR.zip
 unzip unite.v72_dynamic.zip
+
+# Get Phix
+wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/PhiX/Illumina/RTA/PhiX_Illumina_RTA.tar.gz
+tar xf PhiX*Illumina*.tar.gz 
+mv PhiX/Illumina/RTA/Sequence/Bowtie2Index/* Phix/  
+rm -rf Phix/Illumina
+
