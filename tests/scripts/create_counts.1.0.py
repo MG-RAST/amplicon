f5c90853fa8f4bdbd2c489ce78de127b31455c66
Andreas Wilke <wilke@mcs.anl.gov>
2017-12-19 10:45:40 -0600

    Debugging

--- tests/scripts/create_counts.1.0.py
+++ tests/scripts/create_counts.1.0.py
@@ -348,6 +348,7 @@ def main(args):
           for samples in data['noPrimer'] :
            
               # get sample name from file name
+              print(sample)
               logger.debug(samples)
               sample_name = samples[1]['basename']
               # count reads in single file
